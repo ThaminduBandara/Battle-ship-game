8425ef2e5493a5ab1add9d41eb7fbb2226945816
Gimhanihangawaththa <gimhani0327@gmail.com>
2026-03-30 15:57:19 +0530

    protocol is done

--- common/Protocol.java
+++ common/Protocol.java
@@ -1,6 +1,19 @@
-package com.battleship.common;
+package common;
 
 public class Protocol {
-    
-    // Protocol definitions and message types
+
+    // Server -> Client messages
+    public static final String WELCOME = "WELCOME";
+    public static final String PLACE = "PLACE";
+    public static final String START = "START";
+    public static final String YOUR_TURN = "YOUR_TURN";
+    public static final String OPPONENT_TURN = "OPPONENT_TURN";
+    public static final String RESULT = "RESULT";
+    public static final String INFO = "INFO";
+    public static final String WIN = "WIN";
+    public static final String LOSE = "LOSE";
+
+    // Client -> Server messages
+    public static final String PLACED = "PLACED";
+    public static final String ATTACK = "ATTACK";
 }
